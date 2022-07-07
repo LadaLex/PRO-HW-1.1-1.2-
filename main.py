3e585b964b4325229a1242464183f7d28a877df6
LadaLex <90792537+LadaLex@users.noreply.github.com>
2022-07-07 16:55:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ dress = ("Dress", "M", "Red", "20$")
 tshirt = ("T-shirt", "S", "Black", "7$")
 jeans = ("Jeans", "M", "Blue", "32$")
 print(dress, tshirt, jeans, sep="\n")
+
+class Customer:
+    def __init__(self, name, surname, age, phone_number):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.phone_number = phone_number
+    def __str__(self):
+        return "name={}, surname={}, age={}, phone_number={}"
+customer_1 = ("Ivan", "Ivanov", 37, 123456789)
+customer_2 = ("Mary", "Honcharova", 23, 987654321)
+print(customer_1, customer_2, sep="\n")
